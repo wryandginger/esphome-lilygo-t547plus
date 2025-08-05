346d957952e85ca6a7f378ecbc17eff890af928b
wryandginger <77764889+wryandginger@users.noreply.github.com>
2025-08-05 17:08:11 +0000

    Update display.py

--- components/t547/display.py
+++ components/t547/display.py
@@ -49,5 +49,5 @@ async def to_code(config):
 
     cg.add_build_flag("-DBOARD_HAS_PSRAM")
 
-    cg.add_library("Wire", None)  # required by LilyGoEPD47
-    cg.add_library("LilyGoEPD47", repository="https://github.com/Xinyuan-LilyGO/LilyGo-EPD47", version="v0.3.0")
+    cg.add_library("Wire", version="3.1.3")  # required by LilyGoEPD47
+    cg.add_library("LilyGoEPD47", repository="https://github.com/Xinyuan-LilyGO/LilyGo-EPD47", version="v1.0.1")
