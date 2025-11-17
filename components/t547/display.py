6c9069133d8f248905f784315a8aed07411bb5d2
wryandginger <77764889+wryandginger@users.noreply.github.com>
2025-11-16 16:31:56 -0800

    Add files via upload

--- components/t547/display.py
+++ components/t547/display.py
@@ -48,6 +48,3 @@ async def to_code(config):
     cg.add(var.set_greyscale(config[CONF_GREYSCALE]))
 
     cg.add_build_flag("-DBOARD_HAS_PSRAM")
-
-    cg.add_library("Wire", None)  # required by LilyGoEPD47
-    cg.add_library("LilyGoEPD47", repository="https://github.com/Xinyuan-LilyGO/LilyGo-EPD47", version="v0.3.0")
